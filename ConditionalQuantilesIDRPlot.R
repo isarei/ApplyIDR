e0c019954f8d4611d257513682667b851a1b046b
Isabelle Reich <isa@wlan-cl-kit-cs-172-17-13-82.scc.kit.edu>
2019-12-06 13:11:38 +0100

    new Version

--- ConditionalQuantilesIDRPlot.R
+++ ConditionalQuantilesIDRPlot.R
@@ -12,10 +12,10 @@ gdp <- data.frame('dates'=GDP$date, 'X'=GDP$forecast, 'Y'=GDP$observation)
 
 #Choose data set from PointFore 'precip' or 'gdp' (formatted above)
 #Choose corresponding plot title
-#data = precip
-#ti <- "Estimated quantiles of Y|x - Precipitation over London"
-data = gdp
-ti <- "Estimated quantiles of Y|x - GDP data"
+data = precip
+ti <- "Estimated quantiles of Y|x - Precipitation over London"
+#data = gdp
+#ti <- "Estimated quantiles of Y|x - GDP data"
 
 
 #Fit IDR model
@@ -36,10 +36,10 @@ data <- melt(data, id.vars = c('dates', 'X', 'Y'))
 
 #Plot
 p <- ggplot() + 
-  geom_point(data=data, aes(x=X, y=value, color=variable), size=0.5) +
+  geom_line(data=data, aes(x=X, y=value, color=variable), size=0.5) +
   geom_abline(slope = 1, intercept = 0, linetype='dashed') +
   scale_color_discrete(name=expression(alpha), labels=c('0.1', '0.2', '0.3', '0.4', '0.5',
-                                            '0.6', '0.7', '0.8', '0.9')) +
+                                                        '0.6', '0.7', '0.8', '0.9')) +
   xlab('x') + 
   ylab('Y') +
   ggtitle(ti)
