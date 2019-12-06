f9ad7892ce56247fa4266e78de97710d052f2e07
Schmidtpk <pschmidte@gmail.com>
2019-12-06 13:28:37 +0100

    Estimate alpha(x) on grid (instead of on sample x_t)
    -rename alphas to alpha.grid
    -introduce x.grid
    -adapt code such that alpha(x) is estimated on grid

--- EstimateQuantileLevel.R
+++ EstimateQuantileLevel.R
@@ -12,16 +12,18 @@ precip <- data.frame('dates'=dates, 'X'=precipitation$X, 'Y'= precipitation$Y)
 gdp <- data.frame('dates'=GDP$date, 'X'=GDP$forecast, 'Y'=GDP$observation)
 
 #Choose data set from PointFore 'precip' or 'gdp' (formatted above) and title
-#data <- precip
-#ti <- 'Estimated quantile level - Precipitation London'
+# data <- precip
+# ti <- 'Estimated quantile level - Precipitation London'
 data <- gdp
 ti <- 'Estimated quantile level - GDP'
 
 #Initialize variables
 T <- length(data$X)  ##data set length
-alphas <- seq(0.01, 0.99, 0.01)  ##quantile levels
+N.grid.x=100
+alpha.grid <- seq(0.01, 0.99, 0.01)  ##grid of quantile levels
+x.grid <- seq(min(data$X),max(data$X),length.out = N.grid.x) ##grid of forecast levels
 x <- data$X ##forecast
-alpha_x <- numeric(T)  ##vector to save alpha(x)
+alpha_x <- numeric(N.grid.x)  ##vector to save alpha(x)
 
 #Fit IDR model
 fit <- idr(
@@ -30,16 +32,16 @@ fit <- idr(
 )
 
 #Prediction given forecasts x
-prediction <- predict(fit, data = data['X'])
+prediction <- predict(fit, data = data.frame(X=x.grid))
 
 #Quantiles of Y|x
 #quantiles: matrix of dimension Tx99 
 #rows: x_j, cols: quantiles 0.01,0.02,...,0.99
-quantiles <- qpred(prediction, quantiles = alphas)
+quantiles <- qpred(prediction, quantiles = alpha.grid)
 
-#for each x_i (row) find max index where quantile is <= x_i and take the respective alpha
-for (i in seq(1, T, 1)) {
-  alpha_x[i] <- alphas[max(which(quantiles[i,]<=x[i]))]
+#for each x in grid find max index where quantile is <= x_i and take the respective alpha
+for (i in 1:length(x.grid)) {
+  alpha_x[i] <- alpha.grid[max(which(quantiles[i,]<=x.grid[i]))]
 }
 
 #Estimate quantile level with GMM pasrametrically
@@ -54,10 +56,10 @@ res_parametric <- estimate.functional(iden.fct = PointFore::quantiles,
 theta1 <- summary(res_parametric)$coefficients[1,1]
 theta2 <- summary(res_parametric)$coefficients[2,1]
 
-level <- probit_linear(res_parametric$stateVariable, theta = c(theta1, theta2))
+level <- probit_linear(x.grid, theta = c(theta1, theta2))
 
 #save result in data frame and reshape for ggplot
-res <- data.frame('x' = x[3:T], 'alpha_x'=alpha_x[3:T], 'level'=level)
+res <- data.frame('x' = x.grid, 'alpha_x'=alpha_x, 'level'=level)
 res <- melt(res, id.vars = c('x'))
 
 #Plot
