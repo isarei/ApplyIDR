76d06a136426de2da0e3f17ac067eb385287599a
Isabelle Reich <isa@wlan-cl-kit-cs-172-17-13-82.scc.kit.edu>
2019-12-06 13:11:47 +0100

    new Version

--- EstimateQuantileLevel.R
+++ EstimateQuantileLevel.R
@@ -6,14 +6,16 @@ library(isodistrreg)
 library(tidyverse)
 library(reshape2)
 
+#Formate data sets
 dates <-seq(as.Date("2011-07-01"), as.Date("2017/06/30"), "days")
 precip <- data.frame('dates'=dates, 'X'=precipitation$X, 'Y'= precipitation$Y)
+gdp <- data.frame('dates'=GDP$date, 'X'=GDP$forecast, 'Y'=GDP$observation)
 
 #Choose data set from PointFore 'precip' or 'gdp' (formatted above) and title
-data <- precip
-ti <- 'Estimated quantile level - Precipitation London'
-#data <- gdp
-#ti <- 'Estimated quantile level - GDP'
+#data <- precip
+#ti <- 'Estimated quantile level - Precipitation London'
+data <- gdp
+ti <- 'Estimated quantile level - GDP'
 
 #Initialize variables
 T <- length(data$X)  ##data set length
