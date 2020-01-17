78c8a256eae1056dda95375cc5f55e34c1d8b62b
Isabelle Reich <isa@wlan-cl-kit-cs-172-17-13-82.scc.kit.edu>
2020-01-17 10:05:17 +0100

    Both plots

--- plot_expectiles.R
+++ plot_expectiles.R
@@ -5,9 +5,12 @@ library(PointFore)
 library(isotone)
 library(tidyverse)
 library(reshape2)
+library(gtable)
+library(gridExtra)
+library(grid)
 
-y <- precipitation$Y[1:200]
-x <- precipitation$X[1:200]
+y <- precipitation$Y[1:100]
+x <- precipitation$X[1:100]
 
 #P: Sort data such that appropriate to the defined total_order
 y <- y[order(x)]
@@ -35,7 +38,7 @@ plot.data <- data.frame('X'=x, 'Y'=y)
 plot.data <- cbind(plot.data, expectiles)
 plot.data <- melt(plot.data, id.vars = c('X', 'Y'))
 
-p <- ggplot() + 
+p1 <- ggplot() + 
   geom_line(data=plot.data, aes(x=X, y=value, color=variable), 
             size=0.5) +
   geom_abline(slope = 1, intercept = 0, linetype='dashed') +
@@ -44,8 +47,57 @@ p <- ggplot() +
   guides(color = guide_legend(reverse = TRUE)) +
   ylab('y') + 
   ggtitle('Estimated conditional expectiles - Precipitation London')
-p
 
+##Estimate alpha(x)
+alpha.grid <- seq(0.01,0.99,0.01)
+N.grid.x <- 100
+x.grid <- seq(min(x), max(x), length.out=N.grid.x)
+alpha_x <- numeric(N.grid.x)
+expectiles <- matrix(ncol=99, nrow=T)
 
+j=1
+for(i in alpha.grid) {
+  print(i)
+  fit.expectiles <- activeSet(total_order, "asyLS", y = y, weights = w, 
+                              aw = i, bw = 1-i)
+  expectiles[,j] <- fit.expectiles$x
+  j=j+1
+}
+
+for (i in 1:N.grid.x) {
+  alpha_x[i] <- alpha.grid[max(which(expectiles[i,]<=x.grid[i]))]
+}
+
+#Estimate expectile level with GMM parametrically
+res_parametric <- estimate.functional(iden.fct = PointFore::expectiles,
+                                      X = x,
+                                      Y = y,
+                                      stateVariable = x,
+                                      instruments = c("lag(Y,2)", "X"),
+                                      theta0 = c(0,0),
+                                      model = probit_linear)
+
+theta1 <- summary(res_parametric)$coefficients[1,1]
+theta2 <- summary(res_parametric)$coefficients[2,1]
+level <- probit_linear(x.grid, theta = c(theta1, theta2))
+
+res <- data.frame('x' = x.grid, 'alpha_x'=alpha_x, 'level'=level)
+
+res <- melt(res, id.vars = c('x'))
+
+p2 <- ggplot() + 
+  geom_line(data = res, aes(x=x, y=value, col=variable), size=0.5) +
+  #xlim(0,30) +
+  ylim(0,1) +
+  ylab(expression(alpha(x))) +
+  scale_color_discrete(name='Estimated expectile level', labels=c('IDR', 'GMM')) 
+
+#Arrange Plots
+g1 <- ggplotGrob(p1)
+g2 <- ggplotGrob(p2)
+g <- rbind(g1, g2, size="first")
+g$widths <- unit.pmax(g1$widths, g2$widths)
+grid.newpage()
+grid.draw(g)
 
 
